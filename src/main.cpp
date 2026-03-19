963bd17dd46f2424d08a9d36051505cd82ec0277
Vanja <vanjabjor@icloud.com>
2026-03-19 20:39:57 +0100

    test with internal led

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,18 @@
 
 #include "wifiPass.h"
 
-const char* apiUrl = "https://www.omegav.ntnu.no/api/dooropen.php";
+#define LED 2
 
+// const char* ssid = "make";
+// const char* password = "makeallthestuff";
+
+const char* apiUrl = "https://www.omegav.ntnu.no/api/dooropen.php";
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
+
+    pinMode(LED, OUTPUT);       // for testing internal LED
 
     WiFi.mode(WIFI_STA); 
     WiFi.begin(ssid, password);
@@ -37,30 +43,43 @@ void loop() {
     HTTPClient http;
 
     http.begin(apiUrl); 
-    int httpResponseCode = http.GET();      // response is 200 if OK
+    int httpResponseCode = http.GET();        // response is 200 if OK
     if (httpResponseCode > 0) {
       String response = http.getString();
       Serial.println("HTTP Response code: " + String(httpResponseCode));
       Serial.println("Response from API: " + response);
 
-      const size_t capacity = JSON_OBJECT_SIZE(2);
-      DynamicJsonDocument doc(capacity);
+      // const size_t capacity = JSON_OBJECT_SIZE(2) + 20;
+      // DynamicJsonDocument doc(capacity);
+      DynamicJsonDocument doc(256);
       DeserializationError error = deserializeJson(doc, response);
 
       if (!error) {
-        int doorStatus = doc["door"];
+        int doorStatus = doc["open"];
         Serial.println("Door status: " + String(doorStatus));
-
+        if (doorStatus) {
+          digitalWrite(LED,HIGH);
+        } else {
+          digitalWrite(LED,LOW);
+        }
       } else {
-        Serial.println("Failed to parse JSON");
+        Serial.println("Failed to parse JSON: ");
+        Serial.println(error.c_str());
       }
+      
 
   } else {
       Serial.println("Error on HTTP request: " + String(httpResponseCode));
   }
 
-  http.end(); // Free resources
+  http.end(); 
 
-} 
-delay(5000); // Wait 5 seconds before next request
+  } 
+  delay(5000); // Wait 5 seconds before next request
+
+  // testing led blinking
+//   delay(500);
+//   digitalWrite(LED,HIGH);
+//   delay(2000);
+//   digitalWrite(LED,LOW);
 }
